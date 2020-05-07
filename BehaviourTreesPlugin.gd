4c531c8599c2b7e8a890c3367bb2dcbb764ad45f
leoli51 <leo_la_rocca@yahoo.it>
2020-05-07 14:56:27 +0200

    Changed dirs

--- BehaviourTreesPlugin.gd
+++ BehaviourTreesPlugin.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 func _enter_tree():
 	var node_icon = get_editor_interface().get_base_control().get_icon("Node", "EditorIcons")
-	add_custom_type("TaskExecutor", "Node", preload("res://addons/EasyTasks/Engine/TaskExecutor.gd"), node_icon)
-	ProjectSettings["editor/script_templates_search_path"] = "res://addons/EasyTasks/Engine/TaskTemplate/"
+	add_custom_type("TaskExecutor", "Node", preload("res://addons/Godot-BehaviourTrees/Engine/TaskExecutor.gd"), node_icon)
+	ProjectSettings["editor/script_templates_search_path"] = "res://addons/Godot-BehaviourTrees/Engine/TaskTemplate/"
 	
 func _exit_tree():
 	remove_custom_type("TaskExecutor")
